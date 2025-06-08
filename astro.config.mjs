6547120e8934a8770c97fd98b28ad8d4d715f388
gadivalr <levi.sojos7@gmail.com>
2025-06-08 14:02:05 -0400

    op

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+// import rehypeToc from 'rehype-toc';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gadivalr.github.io/',
-  base: '/gadivalr.github.io',
+  //base: '/gadivalr.github.io',
 	integrations: [mdx(), sitemap()],
 	markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    remarkPlugins: [remarkMath, remarkGfm],
+    rehypePlugins: [rehypeKatex, rehypeRaw],
   },
 });
 
