77c74a859c72d9dba9f0e9748e247156ded40c42
gadivalr <levi.sojos7@gmail.com>
2025-06-27 04:51:53 -0400

    add interative-gr

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,6 +7,13 @@ export const translations = {
     blog: "Blog",
     tutorials: "Tutorials",
     SITE_TITLE: SITE_TITLE,
+    P: "Proportional Gain",
+    I: "Integral Gain",
+    D: "Derivative Gain",
+    PV: "Process Variable (PV)",
+    SP: "Set Point (SP)",
+    time: "Time (s)",
+    Ap: "Amplitude",
   },
   es: {
     home: "Inicio",
@@ -14,5 +21,12 @@ export const translations = {
     blog: "Blog",
     tutorials: "Tutoriales",
     SITE_TITLE: "Control System Blog",
+    P: "Ganancia proporcional",
+    I: "Ganancia integral",   
+    D: "Ganancia derivativa",
+    PV: "Respuesta del Sistema (PV)",
+    SP: "Referencia (SP)",
+    time: "Tiempo (s)",
+    Ap: "Amplitud"
   }
 };
